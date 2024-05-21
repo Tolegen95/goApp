b83664d9c564169148acf72ae377c270ced9821d
tolegen <tolegen.ch@gmail.com>
2024-05-21 11:11:37 +0500

    2 коммит: добавлен fails

--- account/main.go
+++ account/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello")
+	log.Println("Starting server...")
+
+	router := gin.Default()
+
+	router.GET("/api/account", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"hello": "Tolegen",
+		})
+	})
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
 }
